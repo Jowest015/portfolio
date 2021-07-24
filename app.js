2e3be51cd3c7ee26aacfa38c759003dfb3ab8b63
John Westover <78867913+Jowest015@users.noreply.github.com>
2021-07-24 12:30:47 -0500

    initial JS basic app setup

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+
+const app = express();
+
+
+//Template engine
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars({
+  layoutsDir: __dirname + '/views/layouts',
+  extname: 'hbs',
+  defaultLayout: 'main'
+}));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+const port = 3000;
+
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
